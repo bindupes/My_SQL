8697f22a48fa949574b5f3a0f5ca4b6533783f17
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-09-08 22:22:32 +0530

    Create Nth highest salary.sql

---
+++ Nth highest salary.sql
@@ -0,0 +1,72 @@
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+
+*/
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE result INT DEFAULT NULL;
+
+    SET result = NULL;
+
+    WHILE i <= N DO
+        SET result = (
+            SELECT MAX(salary)
+            FROM Employee
+            WHERE salary < IFNULL(result, 1000000000)   -- a very large number initially
+        );
+        SET i = i + 1;
+    END WHILE;
+
+    RETURN result;
+END;
