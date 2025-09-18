1a69cbe72d7c4f6a1261fa3b07b3ae5975b0f2f1
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-09-18 09:47:26 +0530

    Update Managers with least report.sql

--- Managers with least report.sql
+++ Managers with least report.sql
@@ -47,6 +47,6 @@ Output:
 SELECT e1.name
 FROM Employee e1
 JOIN Employee e2
-  ON e1.id = e2.managerId
-GROUP BY e1.id, e1.name
-HAVING COUNT(e2.id) >= 5;
+ON e1.id = e2.managerId
+GROUP BY e.id , e1.name
+HAVING COUNT(e2.id) >=5
