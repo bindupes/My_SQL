f6915d629126e02f93fc45f768f1ce17c1c04f5f
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-09-18 12:29:25 +0530

    Update Triangle judgement.sql

--- Triangle judgement.sql
+++ Triangle judgement.sql
@@ -36,12 +36,8 @@ Output:
 | 13 | 15 | 30 | No       |
 | 10 | 20 | 15 | Yes      |
 +----+----+----+---- */
-SELECT 
-    x,
-    y,
-    z,
-    CASE 
-        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
-        ELSE 'No'
-    END AS triangle
-FROM Triangle;
+SELECT x,y,z
+CASE WHEN x+y > z AND x+z > y AND y+z > x THEN "Yes'
+ ELSE "No"
+ END AS triangle
+ FROM Triangle ;
